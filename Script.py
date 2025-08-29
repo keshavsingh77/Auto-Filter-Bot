fc82a2304b0b74b81b54449d083ddbe6cf6d04e3
keshavsingh77 <keshavrajlkr2@gmail.com>
2025-08-29 08:54:03 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -220,24 +220,20 @@ Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
-</b><a href={poster_url}>📥</a><a href={imdb_url}>New {tag} Added</a></b>
+✨ New {tag} Added ✨
 
-<blockquote>✨ ᴛɪᴛʟᴇ : <code>{filename}</code>
+🎬 Title : {filename}
+🎭 Genres : {genres}
+📺 OTT : {ott}
+🎞️ Quality : {quality}
+🎧 Language : {language}
+⭐ Rating : {rating}
 
-
-🎭 ɢᴇɴʀᴇs : <b>{genres}</b>
-📺 ᴏᴛᴛ        : <b>{ott}</b>
-🎞️ ǫᴜᴀʟɪᴛʏ : <b>{quality}</b>
-🎧 ᴀᴜᴅɪᴏ    : <b>{language}</b>
-🔥 ʀᴀᴛɪɴɢ   : <b>{rating}</b>
-{episodes}
-</blockquote>
-
-🌐 Watch/Download More at:  
-👉 (https://filmy4uhd.vercel.app)
-
-🔍 <b>Sᴇᴀʀᴄʜ →</b> {search_link}
+🔎 Search : {search_link}
+🔗 IMDb Link : {custom_link}   ✅ yaha add kiya
 """
+
+
 
 
     IMDB_TEMPLATE_TXT = """
