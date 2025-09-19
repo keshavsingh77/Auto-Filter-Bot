4e5b79bba24946ad4a3f01305e8d7df133a5e11d
keshavsingh77 <keshavrajlkr2@gmail.com>
2025-09-19 15:26:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -223,9 +223,9 @@ else:
     ON_HEROKU = False
 HAS_SSL = bool(getenv('HAS_SSL', True))
 if HAS_SSL:
-    URL = "brave-lilyan-nitish7-614d2364.koyeb.app".format(FQDN)
+    URL = "https://brave-lilyan-nitish7-614d2364.koyeb.app/".format(FQDN)
 else:
-    URL = "brave-lilyan-nitish7-614d2364.koyeb.app".format(FQDN)
+    URL = "https://brave-lilyan-nitish7-614d2364.koyeb.app/".format(FQDN)
 
 # ============================
 # Reactions Configuration
